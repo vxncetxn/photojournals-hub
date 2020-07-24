720cb48c067b01c536647a36e005204e1fdf9fb7
Vance <thevancetan@gmail.com>
2020-07-24 17:56:03 +0800

    Fixed flashing of light theme issue

---
+++ src/html.js
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function () {
+              function setTheme(theme) {
+                document.documentElement.setAttribute("theme", theme);
+              }
+            
+              if (window.localStorage.getItem("theme")) {
+                setTheme(localStorage.getItem("theme"));
+              } else if (window.matchMedia("(prefers-color-scheme: dark").matches) {
+                setTheme("dark");
+              } else {
+                setTheme("light");
+              }
+            })();
+        `,
+          }}
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+};
